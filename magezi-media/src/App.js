ec6298c9e82e65410cca1bdefd2afe1be1f9c81f
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-07-12 07:26:30 +0300

    added

--- magezi-media/src/App.js
+++ magezi-media/src/App.js
@@ -1,12 +1,32 @@
 
 
 
-import './App.css';
-import About from './Podcast.js';
-const Info=()=>{
-  return(
-    <About/>
+// import './App.css';
+// import About from './Podcast.js';
+// const Info=()=>{
+//   return(
+//     <About/>
+//   );
+// }
+
+// export default Info;
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './Podcast';
+
+function App() {
+  return (
+    <Router>
+      <div>
+
+        <Routes>
+          <Route path="/" element={<About/>} />
+          {/* <Route path="/videos" element={<Videos />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default Info;
+export default App;
